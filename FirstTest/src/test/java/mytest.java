fbf2e6682c2dca46e98aa4d57666be929a798f97
Karthik <Karthik@DESKTOP-CVJMP52>
2018-05-05 07:29:17 -0700

    yjfujf

--- FirstTest/src/test/java/mytest.java
+++ FirstTest/src/test/java/mytest.java
@@ -3,5 +3,6 @@ public class mytest {
 public static void main(String[] args) {
 	System.out.println("my first test");
 	System.out.println("my first test");
+	System.out.println("my first test");
 }
 }
